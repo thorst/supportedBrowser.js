1e0b477f5d25f6a506c19d318c22d4204e570eb3
Todd Horst <toddmhorst@yahoo.com>
2014-03-19 13:42:20 -0400

    formatting

--- src/supported.js
+++ src/supported.js
@@ -12,38 +12,39 @@
             os = {},
             browser = {}
         ;
-
+        var i,
+            browserInfo = window.browserInfo;
 
         // valid platform
-        for (var i = 0; i < settings.length; i++) {
-            if (settings[i].os.toLowerCase().indexOf(browserInfo.os.name.toLowerCase()) == 0) {
+        for (i = 0; i < settings.length; i++) {
+            if (settings[i].os.toLowerCase().indexOf(browserInfo.os.name.toLowerCase()) === 0) {
                 result.os = true;
                 os = settings[i];
                 break;
             }
         }
-        if (!result.os) return result;
+        if (!result.os) { return result };
        
 
         // valid platform version
-        for (var i = 0; i < os.versions.length; i++) {
-            if (os.versions[i].toLowerCase().indexOf(browserInfo.os.versionString.toLowerCase()) == 0) {
+        for (i = 0; i < os.versions.length; i++) {
+            if (os.versions[i].toLowerCase().indexOf(browserInfo.os.versionString.toLowerCase()) === 0) {
                 result.osVersion = true;
                 break;
             }
         }
-        if (!result.osVersion) return result;
+        if (!result.osVersion) { return result };
 
 
         // valid browser for platform
-        for (var i = 0; i < os.browsers.length; i++) {
-            if (os.browsers[i].browser.toLowerCase().indexOf(browserInfo.browser.name.toLowerCase()) == 0) {
+        for (i = 0; i < os.browsers.length; i++) {
+            if (os.browsers[i].browser.toLowerCase().indexOf(browserInfo.browser.name.toLowerCase()) === 0) {
                 result.browser = true;
                 browser =os.browsers[i];
                 break;
             }
         }
-        if (!result.browser) return result;
+        if (!result.browser) { return result };
 
 
         // valid version of the browser
